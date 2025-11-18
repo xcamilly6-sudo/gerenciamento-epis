3c70562b9c9dbdc57cc3ea3f8c05789a11c23d8d
camilly xavier de souza <xaviercamilly355@gmail.com>
2025-11-17 21:24:07 -0300

    arquivo epi

--- src/main/java/DTO/ColaboradorDTO.java
+++ src/main/java/DTO/ColaboradorDTO.java
@@ -1,24 +1,16 @@
 package DTO;
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-<<<<<<< Updated upstream
-=======
-import javax.print.DocFlavor;
-
->>>>>>> Stashed changes
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-<<<<<<< Updated upstream
+
 public class ColaboradorDTO {
 
     private int idColaborador;
@@ -26,15 +18,5 @@ public class ColaboradorDTO {
     @NotBlank(message = "Nome do colaborador não pode ser vazio")
     @Size(max = 50, message = "Colaborador não pode ultrapassar 50 caracteres")
     private String nmColaborador;
-=======
 
-public class ColaboradorDTO {
-
-    private int idDepartamento;
-
-    @NotBlank(message= "" )
-    @Size(max= 50, messagem = "")
-    private
-
->>>>>>> Stashed changes
 }
